372c45d07e3292a53dc1a024eea23d6ef4392339
Jia-jia Tang <to.future7@gmail.com>
2020-03-14 11:18:48 -0700

    react exercises start

---
+++ react/01_like_button.js
@@ -0,0 +1,13 @@
+//01 react js
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(
+        <h1>Hello, world!</h1>,
+		domContainer);
+
+/*
+	sample from https://reactjs.org/docs/add-react-to-a-website.html
+	remove function that not introduced on first tutorial. 
+	Since this is the 1st exercise, keep it simple.
+*/
+
