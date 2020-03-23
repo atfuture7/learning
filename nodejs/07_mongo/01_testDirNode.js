6de1d42780c4597803018bc33ed3033a1b567732
Jia-jia Tang <to.future7@gmail.com>
2020-03-22 21:36:54 -0700

    update comment, include important sources

--- nodejs/07_mongo/01_testDirNode.js
+++ nodejs/07_mongo/01_testDirNode.js
@@ -1,6 +1,6 @@
 // Create a mirror directory in nodejs folder. 
 // I need to verify if the path works in reactjs, 
-//since I installed reactjs and mongodb in differet directories
+// since I installed reactjs and mongodb in differet directories
 var dataRep = require('../../data/dataRepNode');
 
 data = new dataRep(); 		//module class defined a little different from reactjs 
@@ -12,4 +12,12 @@ console.log(process.cwd());	//api path asserted
 
 const MongoClient = require(data.mongoApi + 'mongodb').MongoClient;
 const client = new MongoClient(url, {useUnifiedTopology: true});
+
+// 20200321 start my research on MongoDB. 
+// I'm using remote MongoDB service at https://account.mongodb.com/account/login
+// Since I'm not going to expose my file system or administrator information, 
+// I use include file to contain confidential information. 
+// important reference:
+// http://mongodb.github.io/node-mongodb-native/3.5/api/index.html
+// https://docs.mongodb.com/manual/reference/method/
 
